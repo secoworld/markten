8c1dcd8bcfc769beffefe5be715203c20c65399f
longtao <chinaliulongtao@163.com>
2019-05-13 10:46:22 +0800

    fixedtime

--- tomate/tomate/wsgi.py
+++ tomate/tomate/wsgi.py
@@ -8,8 +8,12 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
 
 import os
-
+from os.path import join,dirname,abspath
 from django.core.wsgi import get_wsgi_application
+
+PROJECT_DIR = dirname(dirname(abspath(__file__)))#3
+import sys # 4
+sys.path.insert(0,PROJECT_DIR) # 5
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tomate.settings')
 
